6262b822b2c1bd4808d840295a9a8bdad5579592
Aurelien <aureplusyt@gmail.com>
2025-09-27 21:25:04 +0200

    added skills to project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -32,39 +32,37 @@ const ProjectCard = ({
 }: ProjectCardProps) => {
   return (
     <div
-      className={`physical chain card card-border border-primary rounded-lg lg:card-side bg-base-100 w-2/3 h-80 ${className}`}
+      className={`physical chain card card-border border-primary rounded-lg lg:card-side bg-base-100 w-2/3 h-fit lg:h-80 ${className}`}
     >
-      <figure className="rounded-lg m-4 physical">
+      <figure className="flex-shrink-0 rounded-lg m-4 physical ">
         <img src={imgUrl} alt={title + " image"} draggable={false} />
       </figure>
-      <div className="card-body w-1/2">
+
+      <div className="card-body">
         <WordSeparate
           text={title}
           parentClassName="card-title"
           className="physical h-fit"
           wordGap={1}
         />
+        <div className="   w-full flex items-center justify-center gap-2">
+          {skills.map((skill, index) => (
+            <img
+              key={skill}
+              src={`/logos/Logo${skill}.svg`}
+              className={`physical  w-8 h-8 user-select-none select-none`}
+              alt={`${skill} Logo`}
+              draggable={false}
+            />
+          ))}
+        </div>
         <WordSeparate
           text={description}
           parentClassName="card-description"
           className="physical h-fit"
           wordGap={1}
         />
-        <div className="card-actions chain-container m-5">
-          {skills.map((skill, index) => (
-            <img
-              key={skill}
-              src={`/logos/Logo${skill}.svg`}
-              className={`physical chain ${
-                index === 0 || index === skills.length - 1
-                  ? ""
-                  : "nonconstrained"
-              } w-8 h-8 user-select-none select-none`}
-              alt={`${skill} Logo`}
-              draggable={false}
-            />
-          ))}
-        </div>
+
         {visitUrl != "" && (
           <div className="card-actions justify-end">
             <a
