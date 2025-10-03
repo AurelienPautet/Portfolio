1188bfdeda76a5c6705022a8622d62e72336dcb0
Aurelien <aureplusyt@gmail.com>
2025-10-03 16:44:29 +0200

    transition messing up with displaying the physics

--- src/components/HomeTitle.tsx
+++ src/components/HomeTitle.tsx
@@ -4,7 +4,7 @@ const HomeTitle = () => {
   return (
     <div className="flex  w-full  flex-col justify-center items-center gap-2 mt-32 mb-2">
       <div className="flex flex-row justify-center items-center gap-2 flex-wrap">
-        <h1 className="physical transition-all text-7xl w-fit h-fit font-bold bg-gradient-to-r from-blue-500 via-purple-500  to-blue-500 bg-clip-text text-transparent animate-gradient-scroll bg-[length:200%_auto]">
+        <h1 className="physical text-7xl w-fit h-fit font-bold bg-gradient-to-r from-blue-500 via-purple-500  to-blue-500 bg-clip-text text-transparent animate-gradient-scroll bg-[length:200%_auto]">
           Aurélien
         </h1>
         <WordSeparate
