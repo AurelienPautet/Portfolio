3b35f9868ad02feb1b7c27441a3f724261346045
Aurelien <aureplusyt@gmail.com>
2025-10-05 16:52:53 +0200

    Huge perf boost !!! Using the gpu !!!

--- src/matter/index.js
+++ src/matter/index.js
@@ -225,60 +225,184 @@ document.addEventListener("DOMContentLoaded", function initializePhysics() {
   Runner.run(runner, window.engine);
   Render.run(render);
 
-  function setAbsoluteTransform(PhysicalDomObject) {
-    const x = PhysicalDomObject.physicalBody.getCenter().x;
-    const y = PhysicalDomObject.physicalBody.getCenter().y;
-    const angle = PhysicalDomObject.physicalBody.getAngle();
-    const element = PhysicalDomObject.domElement;
-    let cumulativeMatrix = new DOMMatrix();
-    let parent = PhysicalDomObject.parent;
-    let correction = {
-      x: -PhysicalDomObject.initialPos.x,
-      y: -PhysicalDomObject.initialPos.y,
-    };
+  const parentDataPool = new Array(32);
+  for (let i = 0; i < 32; i++) {
+    parentDataPool[i] = { x: 0, y: 0, cos: 0, sin: 0 };
+  }
 
-    let angleCorrection = PhysicalDomObject.physicalBody.getAngle();
-    const parentChain = [];
-    let i = 0;
-    while (parent !== null) {
-      if (i == 0) {
-        correction.x += +parent.initialPos.x;
-        correction.y += +parent.initialPos.y;
-        angleCorrection -= parent.physicalBody.getAngle();
+  const supportsIndividualTransforms = CSS.supports("translate", "0px");
+  const pendingDOMUpdates = [];
+  let isFlushScheduled = false;
+
+  function flushDOMUpdates() {
+    isFlushScheduled = false;
+    for (let i = 0; i < pendingDOMUpdates.length; i++) {
+      const update = pendingDOMUpdates[i];
+      if (supportsIndividualTransforms) {
+        update.element.style.translate = update.translateStr;
+        update.element.style.rotate = update.rotateStr;
+      } else {
+        update.element.style.transform = update.transformStr;
       }
-      i++;
-      const matrix = new DOMMatrix()
-        .translate(parent.localPos.x, parent.localPos.y)
-        .rotateFromVector(
-          Math.cos(parent.localAngle),
-          Math.sin(parent.localAngle)
-        );
-      parentChain.push({ matrix });
-      parent = parent.parent;
+    }
+    pendingDOMUpdates.length = 0;
+  }
+
+  function scheduleDOMUpdate(element, x, y, angle) {
+    pendingDOMUpdates.push({
+      element: element,
+      translateStr: x + "px " + y + "px",
+      rotateStr: angle + "rad",
+      transformStr:
+        "translate3d(" + x + "px," + y + "px,0)rotateZ(" + angle + "rad)",
+    });
+
+    if (!isFlushScheduled) {
+      isFlushScheduled = true;
+      Promise.resolve().then(flushDOMUpdates);
+    }
+  }
+
+  function setAbsoluteTransform(PhysicalDomObject) {
+    const physicalBody = PhysicalDomObject.physicalBody;
+    const center = physicalBody.getCenter();
+    const x = center.x;
+    const y = center.y;
+    const angle = physicalBody.getAngle();
+    const element = PhysicalDomObject.domElement;
+    const initialPos = PhysicalDomObject.initialPos;
+
+    if (!PhysicalDomObject._transformCache) {
+      PhysicalDomObject._transformCache = {
+        x: -999999,
+        y: -999999,
+        angle: -999999,
+      };
+      element.style.willChange = "transform";
+    }
+    const cache = PhysicalDomObject._transformCache;
+
+    const parent = PhysicalDomObject.parent;
+    if (!parent) {
+      const localPos = PhysicalDomObject.localPos;
+      localPos.x = x;
+      localPos.y = y;
+      PhysicalDomObject.localAngle = angle;
+
+      const finalX = x - initialPos.x;
+      const finalY = y - initialPos.y;
+      const roundedX = Math.round(finalX * 2) / 2;
+      const roundedY = Math.round(finalY * 2) / 2;
+      const roundedAngle = Math.round(angle * 100) / 100;
+
+      if (
+        cache.x !== roundedX ||
+        cache.y !== roundedY ||
+        cache.angle !== roundedAngle
+      ) {
+        cache.x = roundedX;
+        cache.y = roundedY;
+        cache.angle = roundedAngle;
+        scheduleDOMUpdate(element, roundedX, roundedY, roundedAngle);
+      }
+      return;
     }
 
-    for (let i = parentChain.length - 1; i >= 0; i--) {
-      const parentInfo = parentChain[i];
-      cumulativeMatrix = cumulativeMatrix.multiply(parentInfo.matrix);
+    let correctionX = -initialPos.x;
+    let correctionY = -initialPos.y;
+    let angleCorrection = angle;
+    let cumulativeA = 1,
+      cumulativeB = 0,
+      cumulativeC = 0,
+      cumulativeD = 1;
+    let cumulativeE = 0,
+      cumulativeF = 0;
+    let parentCount = 0;
+    let currentParent = parent;
+    let isFirstParent = true;
+
+    while (currentParent !== null && parentCount < 32) {
+      if (isFirstParent) {
+        const parentInitial = currentParent.initialPos;
+        correctionX += parentInitial.x;
+        correctionY += parentInitial.y;
+        angleCorrection -= currentParent.physicalBody.getAngle();
+        isFirstParent = false;
+      }
+
+      const localPos = currentParent.localPos;
+      const localAngle = currentParent.localAngle;
+      const data = parentDataPool[parentCount];
+      data.x = localPos.x;
+      data.y = localPos.y;
+      data.cos = Math.cos(localAngle);
+      data.sin = Math.sin(localAngle);
+
+      parentCount++;
+      currentParent = currentParent.parent;
     }
 
-    const cumulativeTranslateX = cumulativeMatrix.m41;
-    const cumulativeTranslateY = cumulativeMatrix.m42;
-    const cumulativeRotation = Math.atan2(
-      cumulativeMatrix.m12,
-      cumulativeMatrix.m11
-    );
-    const inverseMatrix = cumulativeMatrix.inverse();
+    for (let i = parentCount - 1; i >= 0; i--) {
+      const p = parentDataPool[i];
+      const pCos = p.cos,
+        pSin = p.sin,
+        px = p.x,
+        py = p.y;
 
-    const localPoint = new DOMPoint(x, y).matrixTransform(inverseMatrix);
+      const newA = pCos * cumulativeA - pSin * cumulativeB;
+      const newB = pSin * cumulativeA + pCos * cumulativeB;
+      const newC = pCos * cumulativeC - pSin * cumulativeD;
+      const newD = pSin * cumulativeC + pCos * cumulativeD;
+      const newE = pCos * cumulativeE - pSin * cumulativeF + px;
+      const newF = pSin * cumulativeE + pCos * cumulativeF + py;
+
+      cumulativeA = newA;
+      cumulativeB = newB;
+      cumulativeC = newC;
+      cumulativeD = newD;
+      cumulativeE = newE;
+      cumulativeF = newF;
+    }
+
+    const cumulativeRotation = Math.atan2(cumulativeB, cumulativeA);
+    const det = cumulativeA * cumulativeD - cumulativeB * cumulativeC;
+    if (Math.abs(det) < 1e-10) return;
+
+    const invDet = 1 / det;
+    const invA = cumulativeD * invDet;
+    const invB = -cumulativeB * invDet;
+    const invC = -cumulativeC * invDet;
+    const invD = cumulativeA * invDet;
+    const invE =
+      (cumulativeC * cumulativeF - cumulativeD * cumulativeE) * invDet;
+    const invF =
+      (cumulativeB * cumulativeE - cumulativeA * cumulativeF) * invDet;
+
+    const localX = invA * x + invC * y + invE;
+    const localY = invB * x + invD * y + invF;
     const localAngle = angle - cumulativeRotation;
 
-    PhysicalDomObject.localPos = { x: localPoint.x, y: localPoint.y };
+    const localPos = PhysicalDomObject.localPos;
+    localPos.x = localX;
+    localPos.y = localY;
     PhysicalDomObject.localAngle = localAngle;
 
-    element.style.transform = `translate(${localPoint.x + correction.x}px, ${
-      localPoint.y + correction.y
-    }px) rotate(${angleCorrection}rad)`;
+    const finalX = localX + correctionX;
+    const finalY = localY + correctionY;
+    const roundedX = Math.round(finalX * 2) / 2;
+    const roundedY = Math.round(finalY * 2) / 2;
+    const roundedAngle = Math.round(angleCorrection * 100) / 100;
+
+    if (
+      cache.x !== roundedX ||
+      cache.y !== roundedY ||
+      cache.angle !== roundedAngle
+    ) {
+      cache.x = roundedX;
+      cache.y = roundedY;
+      cache.angle = roundedAngle;
+      scheduleDOMUpdate(element, roundedX, roundedY, roundedAngle);
+    }
   }
 
   document.addEventListener("keydown", function (e) {
