58430e53f8c4a735beedeabd65429c74a0c01efd
Aurélien Pautet <aurelien.pautet@imt-atlantique.net>
2025-12-23 13:13:11 +0100

    fix: white constraint

--- src/matter/mouseInteraction.js
+++ src/matter/mouseInteraction.js
@@ -17,8 +17,8 @@ export function createMouseInteraction(render, engine) {
       damping: 0.1,
       render: {
         visible: true,
-        strokeStyle: "#38bdf8",
-        lineWidth: 1,
+        strokeStyle: "#ffffff",
+        lineWidth: 2,
       },
     },
   });
