1b54c44882a2e50236612ae4f7a91a4bd83404ba
Aurélien Pautet <aurelien.pautet@imt-atlantique.net>
2025-12-23 15:54:57 +0100

    feat: added the SiteGallery project

--- src/components/HomeProjects.tsx
+++ src/components/HomeProjects.tsx
@@ -14,7 +14,7 @@ const HomeProjects = () => {
           className=""
           title="La Fine Equipe"
           date="2025 - Now"
-          description="A full stack mono repo web application for my brother's univ association to manage their events, post articles and more."
+          description="A full stack mono repo web application for my brother's univ association to manage their events, post articles and more. It includes a custom chatbot with a RAG system to answer questions about the association."
           imgUrl="/projectThumbnails/LaFineEquipe.png"
           visitUrl="https://la-fine-equipe.vercel.app"
           gitHubUrl="https://github.com/AurelienPautet/La-Fine-Equipe"
@@ -25,39 +25,38 @@ const HomeProjects = () => {
           className=""
           title="WiiGameBien"
           date="2024 - Now"
-          description="A Javascript and html canvas recreation of the Wii Plays tank game, with a custom engine. Features include multiplayer, level editor, player skins and more."
+          description="A Javascript and html canvas recreation of the Wii Plays tank game, with a custom game and physics engine. Features include multiplayer, level editor, player skins and more."
           imgUrl="/projectThumbnails/WiiGameBien.png"
           visitUrl="https://wiitank.pautet.net"
           gitHubUrl="https://github.com/AurelienPautet/WiiGameBien"
           skills={["Html", "Css", "Js"]}
         />
-
-        <ProjectCard
-          className=" "
-          title="Fourchettas"
-          date="2025"
-          description="A full stack web application used in my engineering school to host foods events where students can order meals made by our cooking club."
-          imgUrl="/projectThumbnails/Fourchettas.png"
-          visitUrl="https://fourchettas-client.vercel.app"
-          gitHubUrl="https://github.com/AurelienPautet/FOURCHETTAS"
-          skills={["Tailwind", "React", "Node", "Postgres"]}
-        />
         <ProjectCard
           className=" "
           title="Transat integration of Fourchettas"
           date="2025"
-          description="Integration of the Fourchettas ordering page into the student made Transat mobile app."
+          description="Integration of the Fourchettas ordering page into the student made Transat mobile app. See the Fourchettas project below for more details."
           imgUrl="/projectThumbnails/TransatFourchettas.png"
-          visitUrl="https://fourchettas-client.vercel.app"
           gitHubUrl="https://github.com/PlugImt/transat-app"
           skills={["React", "Tailwind", "Git"]}
           isNotFinished={true}
         />
         <ProjectCard
+          className=" "
+          title="Fourchettas"
+          date="2025"
+          description="A full stack web application used in my engineering school to host foods events where students can order meals made by our cooking club. It includes an admin panel to manage the events and orders."
+          imgUrl="/projectThumbnails/Fourchettas.png"
+          visitUrl="https://fourchettas-client.vercel.app"
+          gitHubUrl="https://github.com/AurelienPautet/FOURCHETTAS"
+          skills={["Tailwind", "React", "Node", "Postgres"]}
+        />
+
+        <ProjectCard
           className=""
           title="Grammar Corrector"
           date="2025"
-          description="A little Java application that can correct any selected text in any application (shortcut Ctr+Alt+C), using the gemini API from Google."
+          description="A little Java application that can correct any selected text in any application (shortcut Ctr+Alt+C), using the gemini API from Google. I recently ported it to Swift for macOs."
           imgUrl="/projectThumbnails/GrammarCorrector.png"
           gitHubUrl="https://github.com/AurelienPautet/AICorrector"
           skills={["Java"]}
@@ -66,10 +65,19 @@ const HomeProjects = () => {
           className=" "
           title="YouTube Downloader"
           date="2021"
-          description="A simple GUI to download YouTube videos from its url."
+          description="One of my earliest projects, a simple GUI to download YouTube videos from its url."
           imgUrl="/projectThumbnails/YoutubeDownloader.png"
           visitUrl="https://aurelien.pautet.net/software/"
           skills={["Python"]}
+        />
+        <ProjectCard
+          className=" "
+          title="My Early Projects"
+          date="2019 - 2021"
+          description="A collection of my earlies projects, mostly small jsGames"
+          imgUrl="/projectThumbnails/SiteGallery.png"
+          visitUrl="https://aurelien.pautet.net/SiteGallery/"
+          skills={["Css", "Html", "Js"]}
         />
       </div>
     </>
